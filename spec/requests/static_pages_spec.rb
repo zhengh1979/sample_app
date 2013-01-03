b9a122214cdeb8116bbbacc3675b1f9c7915c8a4
zheng hao <zhengh1979@gmail.com>
2013-01-03 14:06:05 -0800

    add contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -48,4 +48,20 @@ describe "Static pages" do
 			:text => "Ruby on Rails Tutorial Sample App | About Us")
 	end
    end
+ 
+   describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('h1', 
+			:text => "Contact")
+	end
+
+    it "should have the title 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+			:text => "Ruby on Rails Tutorial Sample App | Contact")
+	end
+   end
 end
